760e2318809b48e75cfa97becdf50a5d44815c90
bartlomiej <bartlomiejmazur24@gmail.com>
2022-11-23 12:57:28 +0100

    create header with nav

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+
+import React, { useState } from 'react';
+
+import './scss/_Header.scss';
+
+import Menu from './svg/menu.svg';
+import Close from './svg/xmark-solid.svg';
+import CartIcon from './svg/cart-shopping-solid.svg';
+
+import { NavLink } from 'react-router-dom';
+
+
+const Header = () => {
+    const [toggle, setToggle] = useState(false);
+
+    const menuToggleBtn = () => setToggle(!toggle);
+
+    return ( 
+       <header>
+            <div className="menu" onClick={menuToggleBtn}>
+                <img src={Menu} alt="menu icon" width="20"/>
+            </div>
+            <div className="logo">
+                <h1><NavLink to="/">Shop</NavLink></h1>
+            </div>
+            <nav>
+                <ul className={toggle ? "toggle" : ""}>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/product">Product</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/login">Login / Register</NavLink></li>
+                    <li className='close' onClick={menuToggleBtn}>
+                        <img src={Close} alt="close icon" width="20" /></li>
+                </ul>
+                <div className="nav-cart">
+                    <span>0</span>
+                    <NavLink to="/cart">
+                    <img src={CartIcon} alt="cart icon" width="20" />
+                    </NavLink>
+                </div>
+            </nav>
+       </header>
+        );
+}
+ 
+export default Header;
