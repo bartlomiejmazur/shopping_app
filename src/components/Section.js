3c4528be35f3cb3bf66f4682c7f7e6fd1963a6ec
bartlomiej <bartlomiejmazur24@gmail.com>
2022-11-23 13:02:11 +0100

    create Section

---
+++ src/components/Section.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Section = () => {
+    return ( 
+        <h1>Section</h1>
+         );
+}
+ 
+export default Section;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/style.scss';
+import './style.scss';
 import App from './App';
 
 
