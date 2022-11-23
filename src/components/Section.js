6219ef20a9e38e16a20dd27c79854647cda5c430
bartlomiej <bartlomiejmazur24@gmail.com>
2022-11-23 18:29:06 +0100

    Add navigation in section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Products from './section/Products';
+import Details from './section/Details';
+import { Route, Routes } from 'react-router-dom';
+
 
 const Section = () => {
     return ( 
-        <h1>Section</h1>
+        <section>
+        <Routes>
+            <Route path="/product" element={<Products/>} exact/>
+            <Route path="/product/:id" element={<Details/>} />
+        </Routes>
+        </section>
          );
 }
  
