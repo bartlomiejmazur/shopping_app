5dd4d2d43cee03cc726a81f6a01b93c659c90db7
bartlomiej <bartlomiejmazur24@gmail.com>
2022-11-23 18:29:43 +0100

    create Products.js and styling component

---
+++ src/components/section/Products.js
@@ -0,0 +1,35 @@
+import React, {useContext} from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+import {AppContext} from '../AppContext'
+
+import '../scss/_Products.scss';
+
+const Products = () => {
+
+    const {products} = useContext(AppContext);
+    return ( 
+        <div id='product'>
+            {
+                products.map(product => (
+                    <div className="card" key={product.id}>
+                        <NavLink to={`/product/${product.id}`}>
+                            <img className='card-img' src={product.src} alt="img" />
+                        </NavLink>
+                        <div className="content">
+                        <h3>
+                            <NavLink to={`/product/${product.id}`}>{product.title}</NavLink>
+                        </h3>
+                        <span>${product.price}</span>
+                        <p>${product.description}</p>
+                        <button>Add to card</button>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+        );
+}
+ 
+export default Products;
