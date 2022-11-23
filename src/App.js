99f009708bdabbf5833674831dd2f1682f1f4e20
bartlomiej <bartlomiejmazur24@gmail.com>
2022-11-23 12:58:10 +0100

    Add components Header.js and Section.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './scss/style.scss';
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import Header from './components/Header';
+import Section from './components/Section';
 
 function App() {
   return (
-    <div className="App">
-    hello
+    <div className="app">
+      <Router>
+        <Header/>
+        <Section/>
+      </Router>
     </div>
   );
 }
