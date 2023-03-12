8573c57c43e447ce3f1a8946e31baba97b3b13c7
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-12 16:59:25 +0100

    feat: Add new screens to app.

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,22 +6,29 @@ import Cart from './section/Cart';
 import Details from './section/Details';
 import Products from './section/Products';
 import Payment from './section/Payment';
-
-
-
+import About from './screens/About';
+import Contact from './screens/Contact';
+import Login from './screens/Login';
+import Home from './screens/Home';
+import Error from './screens/Error';
 
 const Section = () => {
-    return ( 
-        <section>
-        <Routes>
-            <Route  path="/" element={<Products/>} />
-            <Route  path="/product" element={<Products/>} />
-            <Route exact path="/product/:id" element={<Details/>} />
-            <Route  path="/cart" element={<Cart/>} />
-            <Route  path="/payment" element={<Payment/>} />
-        </Routes>
-        </section>
-         );
-}
- 
+  return (
+    <section>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Products />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/product" element={<Products />} />
+        <Route exact path="/product/:id" element={<Details />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </section>
+  );
+};
+
 export default Section;
