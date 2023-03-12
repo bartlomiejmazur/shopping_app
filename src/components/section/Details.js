209dbb31ea58dd3e05ca20e845f32f1a94aba950
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-12 17:05:23 +0100

    fix: format code & add dep. to useEffect.

--- src/components/section/Details.js
+++ src/components/section/Details.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useContext, useState}  from 'react';
+import React, { useEffect, useContext, useState, useCallback } from 'react';
 import { useParams, NavLink } from 'react-router-dom';
 
 import { AppContext } from '../AppContext';
@@ -6,50 +6,50 @@ import Colors from './Colors';
 
 import '../scss/_Details.scss';
 
-
-
 const Details = () => {
-    const [product, setProduct] = useState([])
-    const {products, addCart} = useContext(AppContext);
-    const {id} = useParams()
+  const [product, setProduct] = useState([]);
+  const { products, addCart } = useContext(AppContext);
+  const { id } = useParams();
 
-   const getProduct = () =>{
-        if(id){
-        const res = products;
-        const data = res.filter(item =>{
-            return item.id === id
-        })
-        setProduct(data)
-        }
-    };
-    useEffect(() =>{
-        getProduct();
-    }, [])
-    
-    return ( 
-        <>
-            {
-                product.map(item =>(
-                <div className="details" key={item.id}>
-                    <img src={item.src} alt="img" />
-                    <div className="box">
-                        <div className="row">
-                            <h2>{item.title}</h2>
-                            <span>${item.price}</span>
-                        </div>
-                        <Colors colors={item.colors}/>
-                        <p>{item.description}</p>
-                        <p>{item.content}</p>
-                        <NavLink to="/cart" className="cart" onClick={() => addCart(item.id)}>
-                            Add to cart
-                        </NavLink>
-                    </div>
+  const getProduct = useCallback(() => {
+    if (id) {
+      const res = products;
+      const data = res.filter((item) => {
+        return item.id === id;
+      });
+      setProduct(data);
+    }
+  }, [products, id]);
 
-                </div>
-                ))
-            }
-        </>
-        );
-}
- 
+  useEffect(() => {
+    getProduct();
+  }, [getProduct]);
+
+  return (
+    <>
+      {product.map((item) => (
+        <div className="details" key={item.id}>
+          <img src={item.src} alt="img" />
+          <div className="box">
+            <div className="row">
+              <h2>{item.title}</h2>
+              <span>${item.price}</span>
+            </div>
+            <Colors colors={item.colors} />
+            <p>{item.description}</p>
+            <p>{item.content}</p>
+            <NavLink
+              to="/cart"
+              className="cart"
+              onClick={() => addCart(item.id)}
+            >
+              Add to cart
+            </NavLink>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
 export default Details;
