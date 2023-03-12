b322f580d61a4becca749c6c66d8020abfb4afd0
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-12 16:37:18 +0100

    fix: Format code.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Header from './components/Header';
-import Section from './components/Section';
-import AppProvider from './components/AppContext';
+import Header from "./components/Header";
+import Section from "./components/Section";
+import AppProvider from "./components/AppContext";
 
 function App() {
   return (
     <AppProvider>
-    <div className="app">
-      <Router>
-        <Header/>
-        <Section/>
-      </Router>
-    </div>
+      <div className="app">
+        <Router>
+          <Header />
+
+          <Section />
+        </Router>
+      </div>
     </AppProvider>
   );
 }
